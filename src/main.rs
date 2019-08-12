336005bac294759c48205e18e6a3b36c283415eb
George Madrid <gmadrid@gmail.com>
2019-08-12 13:16:38 -0400

    Moving camera so that we can see all of the spheres better.

--- src/main.rs
+++ src/main.rs
@@ -69,12 +69,12 @@ fn random_scene() -> Result<Vec<Sphere>> {
 
 fn path_trace(config: &Config) -> Result<()> {
     let mut screen = Screen::new(config.screen_width, config.screen_height, config.scale).unwrap();
-    let camera = Camera::new()?;
-    // let camera = Camera::new_from_to(&Vec3::new(-2.0, 2.0, 1.0),
-    //                                  &Vec3::new(0.0, 0.0, -1.0),
-    //                                  &Vec3::new(0.0, 1.0, 0.0),
-    //                                  45.0,
-    //                                  screen.height() as f32 / screen.width() as f32)?;
+    //let camera = Camera::new()?;
+    let camera = Camera::new_from_to(&Vec3::new(5.0, 2.0, 1.5),
+                                     &Vec3::new(0.0, 0.0, -1.0),
+                                     &Vec3::new(0.0, 1.0, 0.0),
+                                     90.0,
+                                     screen.height() as f32 / screen.width() as f32)?;
 
     screen.one_frame(|fb| {
         let height = fb.height() as f32;
