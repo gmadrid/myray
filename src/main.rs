1ad142310010908288c1e24e01b7656563e75ecb
George Madrid <gmadrid@gmail.com>
2019-08-16 00:16:38 -0400

    All unwrap()s gone from main.rs.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use rays::{
 
 use rays::Progress;
 
-fn color(ray: &Ray, hit_test: &impl HitTest, hue: f32, depth: usize, max_depth: usize) -> Color {
+fn color(ray: &Ray, hit_test: &impl HitTest, hue: f32, depth: usize, max_depth: usize, background: &Color) -> Color {
     if let Some(hit_record) = hit_test.hit_test(ray, 0.001, f32::MAX) {
         if depth >= max_depth {
             return Color::black();
@@ -18,18 +18,19 @@ fn color(ray: &Ray, hit_test: &impl HitTest, hue: f32, depth: usize, max_depth: 
 
         if let Some((scattered, attenuation)) = hit_record.material.scatter(ray, &hit_record) {
             return Color::from(
-                attenuation * color(&scattered, hit_test, hue, depth + 1, max_depth).as_vec(),
+                attenuation * color(&scattered, hit_test, hue, depth + 1, max_depth, background).as_vec(),
             );
         }
-        return Color::black();
+        Color::black()
     } else {
         let unit_direction = ray.direction().unit_vector();
         let t = 0.5 * (unit_direction.y() + 1.0);
-        gradient(t, &Color::white(), &Color::from_hsv(hue, 0.5, 1.0).unwrap())
+        gradient(t, &Color::white(), background)
     }
 }
 
 fn path_trace_inc(config: &Config, world: &World) -> Result<()> {
+    let background = Color::from_hsv(config.hue, 0.5, 1.0)?;
     let mut screen = Screen::new(config.screen_width, config.screen_height, config.scale)?;
     let camera = Camera::new_from_to(
         &config.camera_from,
@@ -49,7 +50,7 @@ fn path_trace_inc(config: &Config, world: &World) -> Result<()> {
         screen.one_frame(|fb| {
             for y in 0..config.screen_height {
                 for x in 0..config.screen_width {
-                    let color = sample_color(config, &world, &camera, x, y, width, height);
+                    let color = sample_color(config, &world, &camera, x, y, width, height, &background);
                     ifb.set(x, y, color);
                 }
             }
@@ -72,14 +73,16 @@ fn sample_color(
     y: usize,
     width: f32,
     height: f32,
+    background: &Color,
 ) -> Color {
     let u = (x as f32 + unit_random()) / width;
     let v = (y as f32 + unit_random()) / height;
     let ray = camera.get_ray(u, v);
-    color(&ray, world, config.hue, 0, config.max_depth)
+    color(&ray, world, config.hue, 0, config.max_depth, background)
 }
 
 fn path_trace(config: &Config, world: &World) -> Result<()> {
+    let background = Color::from_hsv(config.hue, 0.5, 1.0)?;
     let mut screen = Screen::new(config.screen_width, config.screen_height, config.scale)?;
     let camera = Camera::new_from_to(
         &config.camera_from,
@@ -100,7 +103,7 @@ fn path_trace(config: &Config, world: &World) -> Result<()> {
                 let mut color_vec = Vec3::new(0.0, 0.0, 0.0);
                 for _ in 0..config.num_samples {
                     color_vec = color_vec
-                        + sample_color(config, &world, &camera, x, y, width, height).as_vec();
+                        + sample_color(config, &world, &camera, x, y, width, height, &background).as_vec();
                 }
 
                 fb.set(x, y, Color::from(color_vec / config.num_samples as f32));
@@ -143,8 +146,8 @@ fn real_main() -> Result<()> {
 
     if let Some(write) = &config.write {
         let filename = add_extension_if_missing(&write, "yaml");
-        let file = File::create(filename).unwrap(); // TODO
-        serde_yaml::to_writer(file, &world).unwrap(); // TODO
+        let file = File::create(filename)?;
+        serde_yaml::to_writer(file, &world)?;
     }
 
     path_trace_inc(&config, &world)?;
